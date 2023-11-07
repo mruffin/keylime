9e09042bc776ea6e90eec363d2bf546f5439923f
mruffin <margieruffin60@gmail.com>
2023-11-07 14:47:40 -0600

    added record generation

--- Runtime_Policy_Generation/runtimeconf.py
+++ Runtime_Policy_Generation/runtimeconf.py
@@ -1,11 +1,13 @@
 import datetime
 
 mainVars = {
-    'tmpDir' : '/tmp/allowlist_config/',
-    'mainPath' : '/tmp/allowlist_config/MainRepo',
-    'updatePath' : '/tmp/allowlist_config/UpdatePackage',
-    'securityPath' : '/tmp/allowlist_config/SecurityPackage',
-    'allowlist': "/tmp/allowlist_config/allowlist_" + str(datetime.date.today()) + ".txt"
+    'tmpDir' : '/home/mruffin2/Research/Keylime/keylime/Runtime_Policy_Generation/allowlist_config/',
+    'mainPath' : '/home/mruffin2/Research/Keylime/keylime/Runtime_Policy_Generation/allowlist_config/MainRepo',
+    'updatePath' : '/home/mruffin2/Research/Keylime/keylime/Runtime_Policy_Generation/allowlist_config/UpdatePackage',
+    'securityPath' : '/home/mruffin2/Research/Keylime/keylime/Runtime_Policy_Generation/allowlist_config/SecurityPackage',
+    'allowlistOrig': "/home/mruffin2/Research/Keylime/keylime/Runtime_Policy_Generation/allowlist_config/allowlist_orig_" + str(datetime.date.today()) + ".txt",
+    'allowlistUpdate': "/home/mruffin2/Research/Keylime/keylime/Runtime_Policy_Generation/allowlist_config/allowlist_main_" + str(datetime.date.today()) + ".txt",
+    'recordName' : "/home/mruffin2/Research/Keylime/keylime/Runtime_Policy_Generation/allowlist_config/allowlistRecord.json"
 
 }
 
@@ -13,3 +15,4 @@ mirror = {
     "mirror_file_path": "http://130.126.137.0/ubuntu/mirror/archive.ubuntu.com/ubuntu/dists/",
     "mirror_main_path" : "http://130.126.137.0/ubuntu/mirror/archive.ubuntu.com/ubuntu/"
 }
+
