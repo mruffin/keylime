3a9225ab4748ee0abc02eccb8848f9d5caa8940c
mruffin <margieruffin60@gmail.com>
2023-11-29 13:34:55 -0600

    updating runtime policy generation

--- Runtime_Policy_Generation/runtimeconf.py
+++ Runtime_Policy_Generation/runtimeconf.py
@@ -5,8 +5,8 @@ mainVars = {
     'mainPath' : '/home/mruffin2/Research/Keylime/keylime/Runtime_Policy_Generation/allowlist_config/MainRepo',
     'updatePath' : '/home/mruffin2/Research/Keylime/keylime/Runtime_Policy_Generation/allowlist_config/UpdatePackage',
     'securityPath' : '/home/mruffin2/Research/Keylime/keylime/Runtime_Policy_Generation/allowlist_config/SecurityPackage',
-    'allowlistOrig': "/home/mruffin2/Research/Keylime/keylime/Runtime_Policy_Generation/allowlist_config/allowlist_orig_" + str(datetime.date.today()) + ".txt",
-    'allowlistUpdate': "/home/mruffin2/Research/Keylime/keylime/Runtime_Policy_Generation/allowlist_config/allowlist_main_" + str(datetime.date.today()) + ".txt",
+    'allowlistOrig': "/home/mruffin2/Research/Keylime/keylime/Runtime_Policy_Generation/allowlist_config/Allowlists/allowlist_orig_" + str(datetime.date.today()) + ".txt",
+    'allowlistUpdate': "/home/mruffin2/Research/Keylime/keylime/Runtime_Policy_Generation/allowlist_config/Allowlists/allowlist_updated_" + str(datetime.date.today()) + ".txt",
     'recordName' : "/home/mruffin2/Research/Keylime/keylime/Runtime_Policy_Generation/allowlist_config/allowlistRecord.json",
     'recordNameTemp' : "/home/mruffin2/Research/Keylime/keylime/Runtime_Policy_Generation/allowlist_config/allowlistRecordTemp.json",
     'recordNameZip' : "/home/mruffin2/Research/Keylime/keylime/Runtime_Policy_Generation/allowlist_config/allowlistRecord.zip"
